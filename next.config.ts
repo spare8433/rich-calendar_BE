efd7fe591bff7d790d6df826798f6ebce2aa4b02
Byeong <byeongchan8433@gmail.com>
2025-02-07 01:22:51 +0900

    REFACTOR: change response header setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,53 @@
 import type { NextConfig } from "next";
+import { env } from "process";
+
+const allowedOrigin = env.ALLOWED_ORIGIN || "";
+
+const BASIC_HEADERS = [
+  {
+    key: "Access-Control-Allow-Origin",
+    value: allowedOrigin,
+  },
+  {
+    key: "Access-Control-Allow-Methods",
+    value: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  },
+  {
+    key: "Access-Control-Allow-Headers",
+    value: "Content-Type",
+  },
+];
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          ...BASIC_HEADERS,
+        ],
+      },
+      {
+        source: "/api/auth/:path*",
+        headers: BASIC_HEADERS,
+      },
+      {
+        source: "/api/auth/login",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          ...BASIC_HEADERS,
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
